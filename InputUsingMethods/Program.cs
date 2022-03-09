3d718b39237be5b27106bfea6ad2b7057dd163fe
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-09 17:43:12 +0530

    added welcome message

--- InputUsingMethods/Program.cs
+++ InputUsingMethods/Program.cs
@@ -6,9 +6,7 @@ namespace InputUsingMethods
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Welcome to InputUsingMethods Project");
         }
-
-        
     }
 }
