8edff7a4a53f1306a2ccbb6dd29a44e6e324cd53
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-09 17:35:23 +0530

    added empty project

---
+++ InputUsingMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InputUsingMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        
+    }
+}
